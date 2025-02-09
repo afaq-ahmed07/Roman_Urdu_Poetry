f66d224eedd1f3522b66027297334a64f91a0f93
afaq-ahmed07 <afaqshahbaz9@gmail.com>
2025-02-09 16:32:15 +0500

    fixed errors

--- model.py
+++ model.py
@@ -2,20 +2,16 @@
 import torch
 import torch.nn as nn
 
-# Define BiLSTM Poetry Model with BiLSTM Layers
+# Define BiLSTM Poetry Model
 class BiLSTMPoetryModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim=128, hidden_dim=256, num_layers=2):
         super(BiLSTMPoetryModel, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
-        # First BiLSTM layer
-        self.bilstm1 = nn.LSTM(embedding_dim, hidden_dim, num_layers, batch_first=True, bidirectional=True)
-        # Fully connected layer
-        self.fc = nn.Linear(hidden_dim * 2, vocab_size)  # Output layer
+        self.bilstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_dim * 2, vocab_size)  # Multiply hidden_dim by 2 due to bidirectionality
 
     def forward(self, x):
         x = self.embedding(x)
-        # First BiLSTM layer
-        lstm_out, _ = self.bilstm1(x)
-        # Fully connected layer
-        output = self.fc(lstm_out[:, -1, :])
+        lstm_out, _ = self.bilstm(x)
+        output = self.fc(lstm_out[:, -1, :])  # Use the last time step's output
         return output
